5b077edadde6b9978e767b8756e0fe963f3fdc14
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-03-31 22:17:24 -0600

    Lab 4 Done

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView
+} from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
 
-export default function App() {
+
+function App() {
+  
+  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
+  
+  const addTask = (taskText) => {
+    
+    setTasks([...tasks, taskText]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      
+      <ToDoList tasks={tasks}/>
+      
+      <ToDoForm addTask={addTask}/>
+
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+export default App;
